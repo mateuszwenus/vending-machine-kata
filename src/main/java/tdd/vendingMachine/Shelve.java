b3f1f5d958a86764f5f2e9467d83fd9ffc90e0ae
Mateusz Wenus <mateusz.wenus@gmail.com>
2016-09-30 23:06:30 +0200

    make test pass: shelve should throw exception when number of products is negative

--- src/main/java/tdd/vendingMachine/Shelve.java
+++ src/main/java/tdd/vendingMachine/Shelve.java
@@ -3,5 +3,8 @@ package tdd.vendingMachine;
 public class Shelve {
 
 	public Shelve(int numberOfProducts, Product product) {
+		if (numberOfProducts < 0) {
+			throw new IllegalArgumentException("number of products must not be < 0");
+		}
 	}
 }
