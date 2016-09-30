39a21f575e44df843b0cce20d56212e90bd54ce8
Mateusz Wenus <mateusz.wenus@gmail.com>
2016-09-30 22:57:19 +0200

    fix typos

--- src/main/java/tdd/vendingMachine/Product.java
+++ src/main/java/tdd/vendingMachine/Product.java
@@ -4,13 +4,13 @@ public class Product {
 
 	public Product(String name, Money price) {
 		if (name == null) {
-			throw new NullPointerException("name must not ne null");
+			throw new NullPointerException("name must not be null");
 		}
 		if (name.matches("\\s*")) {
 			throw new IllegalArgumentException("name must not be blank");
 		}
 		if (price == null) {
-			throw new NullPointerException("price must not ne null");
+			throw new NullPointerException("price must not be null");
 		}
 	}
 }
