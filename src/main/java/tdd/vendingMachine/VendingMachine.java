b4dde09f1dd899c71936af070e2a6c74b3dfeaab
Mateusz Wenus <mateusz.wenus@gmail.com>
2016-09-30 23:11:13 +0200

    make test pass: vending machine has shelves

--- src/main/java/tdd/vendingMachine/VendingMachine.java
+++ src/main/java/tdd/vendingMachine/VendingMachine.java
@@ -1,5 +1,10 @@
 package tdd.vendingMachine;
 
+import java.util.List;
+
 public class VendingMachine {
+
+	public VendingMachine(List<Shelve> shelves) {
+	}
 
 }
--- src/test/java/tdd/vendingMachine/VendingMachineTest.java
+++ src/test/java/tdd/vendingMachine/VendingMachineTest.java
@@ -2,16 +2,10 @@ package tdd.vendingMachine;
 
 import java.util.Arrays;
 
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 public class VendingMachineTest {
 
-    @Test
-    public void just_a_stupid_passing_test_to_ensure_that_tests_are_run() {
-        Assertions.assertThat(new VendingMachine()).isNotNull();
-    }
-    
     @Test
 	public void VendingMachine_has_shelves() {
 		// when
