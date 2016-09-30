5bebbe9683bbb16d3f58afa497d85d79bec4e26c
Mateusz Wenus <mateusz.wenus@gmail.com>
2016-09-30 23:13:25 +0200

    make test pass: vending machine should throw exception when shelves are null

--- src/main/java/tdd/vendingMachine/VendingMachine.java
+++ src/main/java/tdd/vendingMachine/VendingMachine.java
@@ -5,6 +5,9 @@ import java.util.List;
 public class VendingMachine {
 
 	public VendingMachine(List<Shelve> shelves) {
+		if (shelves == null) {
+			throw new NullPointerException("shelves must not be null");
+		}
 	}
 
 }
