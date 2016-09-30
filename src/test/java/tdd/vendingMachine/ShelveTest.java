d0d5c36ac74368f0d3d0b25eca168f0404ac3692
Mateusz Wenus <mateusz.wenus@gmail.com>
2016-09-30 23:05:46 +0200

    add test: shelve should throw exception when number of products is negative

--- src/test/java/tdd/vendingMachine/ShelveTest.java
+++ src/test/java/tdd/vendingMachine/ShelveTest.java
@@ -1,5 +1,7 @@
 package tdd.vendingMachine;
 
+import static org.junit.Assert.fail;
+
 import org.junit.Test;
 
 public class ShelveTest {
@@ -8,5 +10,16 @@ public class ShelveTest {
 	public void Shelve_contains_multiple_products_of_the_same_type() {
 		// when
 		new Shelve(5, new Product("Mineral water", new Money(100)));
+	}
+	
+	@Test
+	public void Shelve_should_throw_exception_when_number_of_products_is_negative() {
+		try {
+			// when
+			new Shelve(-1, new Product("Mineral water", new Money(100)));
+			fail();
+		} catch (IllegalArgumentException expected) {
+			// then
+		}
 	}
 }
