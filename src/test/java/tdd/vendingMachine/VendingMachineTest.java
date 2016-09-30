f59ae1be01b7eaf3ec8ef23b1efe386cedca081d
Mateusz Wenus <mateusz.wenus@gmail.com>
2016-09-30 23:14:08 +0200

    add test: vending machine should throw exception when shelves is empty list

--- src/test/java/tdd/vendingMachine/VendingMachineTest.java
+++ src/test/java/tdd/vendingMachine/VendingMachineTest.java
@@ -3,6 +3,7 @@ package tdd.vendingMachine;
 import static org.junit.Assert.fail;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 import org.junit.Test;
 
@@ -21,6 +22,17 @@ public class VendingMachineTest {
     		new VendingMachine(null);
     		fail();
     	} catch (NullPointerException expected) {
+    		// then
+    	}
+	}
+    
+    @Test
+	public void VendingMachine_should_throw_exception_when_shelves_is_empty_list() {
+    	try {
+    		// when
+    		new VendingMachine(Collections.emptyList());
+    		fail();
+    	} catch (IllegalArgumentException expected) {
     		// then
     	}
 	}
