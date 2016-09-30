d0a417f46b7207c923005eed1e3def260c83dbd0
Mateusz Wenus <mateusz.wenus@gmail.com>
2016-09-30 23:12:34 +0200

    add test: vending machine should throw exception when shelves are null

--- src/test/java/tdd/vendingMachine/VendingMachineTest.java
+++ src/test/java/tdd/vendingMachine/VendingMachineTest.java
@@ -1,5 +1,7 @@
 package tdd.vendingMachine;
 
+import static org.junit.Assert.fail;
+
 import java.util.Arrays;
 
 import org.junit.Test;
@@ -10,5 +12,16 @@ public class VendingMachineTest {
 	public void VendingMachine_has_shelves() {
 		// when
     	new VendingMachine(Arrays.asList(new Shelve(1, new Product("Mineral water", new Money(100)))));
+	}
+    
+    @Test
+	public void VendingMachine_should_throw_exception_when_shelves_are_null() {
+    	try {
+    		// when
+    		new VendingMachine(null);
+    		fail();
+    	} catch (NullPointerException expected) {
+    		// then
+    	}
 	}
 }
