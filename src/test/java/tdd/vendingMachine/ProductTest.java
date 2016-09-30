ed5947015e3a3582ffcc3ae60b456dbc843e5587
Mateusz Wenus <mateusz.wenus@gmail.com>
2016-09-30 22:49:33 +0200

    add test: product has a name and a price

--- src/test/java/tdd/vendingMachine/ProductTest.java
+++ src/test/java/tdd/vendingMachine/ProductTest.java
@@ -7,16 +7,16 @@ import org.junit.Test;
 public class ProductTest {
 
 	@Test
-	public void Product_has_a_name() {
+	public void Product_has_a_name_and_a_price() {
 		// when
-		new Product("Mineral water");
+		new Product("Mineral water", new Money(100));
 	}
 
 	@Test
 	public void Product_should_throw_exception_when_name_is_null() {
 		try {
 			// when
-			new Product(null);
+			new Product(null, new Money(100));
 			fail();
 		} catch (NullPointerException expected) {
 			// then
