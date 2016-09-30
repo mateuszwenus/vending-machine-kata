a63244a580b20d1358343a4d875cc0359839999b
Mateusz Wenus <mateusz.wenus@gmail.com>
2016-09-30 23:10:34 +0200

    add test: vending machine has shelves

--- src/test/java/tdd/vendingMachine/VendingMachineTest.java
+++ src/test/java/tdd/vendingMachine/VendingMachineTest.java
@@ -1,5 +1,7 @@
 package tdd.vendingMachine;
 
+import java.util.Arrays;
+
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
@@ -9,4 +11,10 @@ public class VendingMachineTest {
     public void just_a_stupid_passing_test_to_ensure_that_tests_are_run() {
         Assertions.assertThat(new VendingMachine()).isNotNull();
     }
+    
+    @Test
+	public void VendingMachine_has_shelves() {
+		// when
+    	new VendingMachine(Arrays.asList(new Shelve(1, new Product("Mineral water", new Money(100)))));
+	}
 }
