a5b7b101dfaeee4014f62d961bbae7f04eccb4e8
Mateusz Wenus <mateusz.wenus@gmail.com>
2016-09-30 23:00:45 +0200

    make test pass: money's constructor should throw exception when amount is negative

--- src/main/java/tdd/vendingMachine/Money.java
+++ src/main/java/tdd/vendingMachine/Money.java
@@ -3,5 +3,8 @@ package tdd.vendingMachine;
 public class Money {
 
 	public Money(int amountGr) {
+		if (amountGr < 0) {
+			throw new IllegalArgumentException("amountGr must not be < 0");
+		}
 	}
 }
