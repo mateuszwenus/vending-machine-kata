ba965345fc23414f22779aca4dbd7970a715fe67
Mateusz Wenus <mateusz.wenus@gmail.com>
2016-10-02 22:09:09 +0200

    code formatting

--- src/test/java/tdd/vendingMachine/VendingMachineTest.java
+++ src/test/java/tdd/vendingMachine/VendingMachineTest.java
@@ -11,41 +11,42 @@ import org.junit.Test;
 
 public class VendingMachineTest {
 
-    @Test
+	@Test
 	public void VendingMachine_has_shelves() {
 		// when
-    	new VendingMachine(Arrays.asList(new Shelve(1, new Product("Mineral water", new Money(100)))));
+		new VendingMachine(Arrays.asList(new Shelve(1, new Product("Mineral water", new Money(100)))));
 	}
-    
-    @Test
+
+	@Test
 	public void VendingMachine_should_throw_exception_when_shelves_are_null() {
-    	try {
-    		// when
-    		new VendingMachine(null);
-    		fail();
-    	} catch (NullPointerException expected) {
-    		// then
-    	}
+		try {
+			// when
+			new VendingMachine(null);
+			fail();
+		} catch (NullPointerException expected) {
+			// then
+		}
 	}
-    
-    @Test
+
+	@Test
 	public void VendingMachine_should_throw_exception_when_shelves_is_empty_list() {
-    	try {
-    		// when
-    		new VendingMachine(Collections.emptyList());
-    		fail();
-    	} catch (IllegalArgumentException expected) {
-    		// then
-    	}
+		try {
+			// when
+			new VendingMachine(Collections.emptyList());
+			fail();
+		} catch (IllegalArgumentException expected) {
+			// then
+		}
 	}
-    
-    @Test
+
+	@Test
 	public void should_display_product_price_when_valid_shelve_number_is_selected() {
 		// given
-    	VendingMachine vendingMachine = new VendingMachine(Arrays.asList(new Shelve(1, new Product("Mineral water", new Money(100)))));
+		VendingMachine vendingMachine = new VendingMachine(Arrays.asList(new Shelve(1, new Product("Mineral water", new Money(100)))));
 		// when
-    	vendingMachine.selectShelve(0);
+		vendingMachine.selectShelve(0);
 		// then
-    	assertThat(vendingMachine.getDisplay(), is("1.00"));
+		assertThat(vendingMachine.getDisplay(), is("1.00"));
 	}
+
 }
