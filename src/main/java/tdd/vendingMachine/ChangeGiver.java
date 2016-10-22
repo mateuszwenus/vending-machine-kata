9bdbe40d73c1ceb1a827c1984c05bf11b2d47c9a
Mateusz Wenus <mateusz.wenus@gmail.com>
2016-10-22 22:36:07 +0200

    make test pass: change giver should give change using one coin

--- src/main/java/tdd/vendingMachine/ChangeGiver.java
+++ src/main/java/tdd/vendingMachine/ChangeGiver.java
@@ -1,10 +1,20 @@
 package tdd.vendingMachine;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class ChangeGiver {
 
 	public List<Coin> giveChange(int amountToGive, List<Coin> availableCoins) {
-		throw new IllegalArgumentException();
+		if (amountToGive <= 0) {
+			throw new IllegalArgumentException();
+		}
+		for (Coin coin : availableCoins) {
+			if (coin.toMoney().equals(new Money(amountToGive))) {
+				return Arrays.asList(coin);
+			}
+		}
+		return Collections.emptyList();
 	}
 }
