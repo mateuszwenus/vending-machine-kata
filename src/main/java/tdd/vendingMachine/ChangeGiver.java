9f93efc1c41d760b5f642a8ec06573fe671ef2ad
Mateusz Wenus <mateusz.wenus@gmail.com>
2016-10-22 22:29:06 +0200

    make test pass: change giver should throw exception when amount to give is nonpositive

--- src/main/java/tdd/vendingMachine/ChangeGiver.java
+++ src/main/java/tdd/vendingMachine/ChangeGiver.java
@@ -5,8 +5,6 @@ import java.util.List;
 public class ChangeGiver {
 
 	public void giveChange(int amountToGive, List<Coin> availableCoins) {
-		// TODO Auto-generated method stub
-		
+		throw new IllegalArgumentException();
 	}
-
 }
