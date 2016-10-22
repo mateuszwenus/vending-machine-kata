a7b5d43315fdc06ad3b962fa4c00f5eb032c59bc
Mateusz Wenus <mateusz.wenus@gmail.com>
2016-10-22 22:28:35 +0200

    make test compile: change giver should throw exception when amount to give is nonpositive

---
+++ src/main/java/tdd/vendingMachine/ChangeGiver.java
@@ -0,0 +1,12 @@
+package tdd.vendingMachine;
+
+import java.util.List;
+
+public class ChangeGiver {
+
+	public void giveChange(int amountToGive, List<Coin> availableCoins) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
