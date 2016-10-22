368b3d66aa45f81a20d721f2be8baa31b2d5a7a7
Mateusz Wenus <mateusz.wenus@gmail.com>
2016-10-22 22:32:13 +0200

    add test: change giver should give change using one coin

--- src/test/java/tdd/vendingMachine/ChangeGiverTest.java
+++ src/test/java/tdd/vendingMachine/ChangeGiverTest.java
@@ -1,8 +1,14 @@
 package tdd.vendingMachine;
 
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -19,5 +25,17 @@ public class ChangeGiverTest {
 		} catch (IllegalArgumentException expected) {
 			// then
 		}
+	}
+
+	@Test
+	public void should_give_change_using_one_coin() {
+		// given
+		ChangeGiver changeGiver = new ChangeGiver();
+		// when
+		List<Coin> change = changeGiver.giveChange(200, Arrays.asList(Coin.ONE_ZL, Coin.TWO_ZL));
+		// then
+		assertThat(change, notNullValue());
+		assertThat(change.size(), is(1));
+		assertThat(change, hasItem(Coin.TWO_ZL));
 	}
 }
